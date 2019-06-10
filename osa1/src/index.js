b122490f834e01b699ffa9b540552e7dca418b8b
matiaslang <matiaslang@icloud.com>
2019-06-10 22:09:36 +0300

    Added anekdot generator and part 1.6

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -9,12 +9,17 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+const [first, second, last] = props.parts
   return (
-      <>
-        <Part name = {props.name} exercises = {props.exercises}/>
-      </>
+    <>
+      <Part name = {first.name} exercises = {first.exercises}/>
+      <Part name = {second.name} exercises = {second.exercises}/>
+      <Part name = {last.name} exercises = {last.exercises}/>  
+    </>   
   )
 }
+
+
 const Part = (props) => {
     return(
     <div>
@@ -25,11 +30,12 @@ const Part = (props) => {
     )
 }
 const Total = (props) => {
-    
+  const [first, second, last] = props.parts
+  const total = first.exercises + second.exercises + last.exercises
     return(
     <div>
         <p>
-            yhteensä {props.number} tehtävää
+            yhteensä {total} tehtävää
         </p>
     </div>
         
@@ -38,43 +44,30 @@ const Total = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack -sovelluskehitys'
-  const part1 = {
-    name: 'Reactin perusteet',
-    exercises: 10
+  const course = {
+    name: 'Half Stack -sovelluskehitys',
+    parts: [
+      {
+        name: 'Reactin perusteet',
+        exercises: 10
+      },
+      {
+        name: 'Tiedonvälitys propseilla',
+        exercises: 7
+      },
+      {
+        name: 'Komponenttien tila',
+        exercises: 14
+      }
+    ]
   }
-  const part2 = {
-    name: 'Tiedonvälitys propseilla',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'Komponenttien tila',
-    exercises: 14
-  }
-
   return (
     <div>
-      <Header course={course} />
-      <Content name = {part1.name} exercises = {part1.exercises}/>
-      <Content name = {part2.name} exercises = {part2.exercises}/>
-      <Content name = {part3.name} exercises = {part3.exercises}/>
-      <Total number = {part1.exercises + part2.exercises + part3.exercises} />
+      <Header course = {course.name} />
+      <Content parts = {course.parts}/>
+      <Total parts = {course.parts}/>
     </div>
-    /*
-    <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>yhteensä {exercises1 + exercises2 + exercises3} tehtävää</p>
-    </div>
-    */
+    
     )
 }
 
