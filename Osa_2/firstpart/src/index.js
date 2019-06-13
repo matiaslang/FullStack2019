2f38f18bf099ad94f750a0062553e7922571add0
matiaslang <matiaslang@icloud.com>
2019-06-13 20:13:51 +0300

    last phase

--- Osa_2/firstpart/src/index.js
+++ Osa_2/firstpart/src/index.js
@@ -1,47 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import AllCourses from './AllCourses'
 
-const Note = ({note, key, exercises }) => {
-  return (
-    <p>{note}   {exercises}</p>
-  )
-}
-const Total = ({total}) => (
-  <h4>Total of {total} exercises</h4>
-)
-
-const AllCourses = (props) => {
-
-  const courses = () => props.course.map(parts => 
-    <Course course = {parts.parts} name = {parts.name}/>)
-  
-  
-  return(
-    <div>{courses()}</div>
-  )
-}
-
-const Course = (props) => {
-  
-  const reducer = (accumulator, currentValue) => accumulator + currentValue
-  const rows = () => props.course.map(parts => 
-    <Note 
-      key={parts.id}
-      note={parts.name}
-      exercises={parts.exercises}
-    />
-  )
-  console.log("OSAT OVAT" , props.course.name)
-  var total = props.course.map(parts => parts.exercises)
-  total = total.reduce(reducer)
-  return(
-  <div>
-    <h1>{props.name}</h1>
-    {rows()}
-    <Total total={total}/>
-  </div>
-  )
-}
 
 const App = () => {
   const courses = [
