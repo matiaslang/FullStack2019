f2ce62abe88720cd06fe779aaa73e150b8102ed0
Matias Lang <matiaslang@icloud.com>
2019-09-30 22:53:09 +0300

    Dividing to smaller pieces

---
+++ Osa_3/Bloglist/app.js
@@ -0,0 +1,24 @@
+const config = require('./utils/config')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+
+console.log('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(bodyParser.json())
+const notesRouter = require('./controllers/blogs')
+app.use('/api/blogs', notesRouter)
+
+module.exports = app
--- Osa_3/Bloglist/index.js
+++ Osa_3/Bloglist/index.js
@@ -1,3 +1,17 @@
+const app = require('./app')
+const http = require('http')
+const config = require('./utils/config.js')
+
+const server = http.createServer(app)
+
+server.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
+})
+
+
+
+
+/*
 require('dotenv').config()
 const http = require('http')
 const express = require('express')
@@ -9,39 +23,18 @@ const mongoose = require('mongoose')
 let PORT = process.env.PORT
 let MONGODB_URI = process.env.MONGODB_URI
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
 
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true , useUnifiedTopology: true})
 
-app.use(cors())
-app.use(bodyParser.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+
+*/
